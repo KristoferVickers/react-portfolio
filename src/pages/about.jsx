1f004eef6a3b91998b9cada144bd44efeb3eb7ce
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-02-19 10:12:59 -0600

    Added jsx work for the about,contact, and resume

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I am a 24 year old that lives in Springfield Mo. I am just trying to get my feet wet in the world of programming.</p>
+      <p>...</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions or inquiries, please feel free to contact us:</p>
+      <ul>
+        <li>Email: Kristofervickers2018@gmail.com</li>
+        <li>Git Hub: https://github.com/KristoferVickers </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
