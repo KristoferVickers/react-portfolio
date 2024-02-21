e83909ef18758e1c28827ab97b0578161e86000e
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-02-20 18:34:47 -0600

    Got the "Contact Me" To work in the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,16 @@
 const Footer = () => {
     return (
-        <footer>created with react and vite</footer>
+        <footer>
+    
+        <h2>Contact me</h2>
+        <p>If you have any questions or inquiries, please feel free to contact me:</p>
+        <ul>
+        <li>Email: Kristofervickers2018@gmail.com</li>
+        <li>GitHub: <a href="https://github.com/KristoferVickers">https://github.com/KristoferVickers</a></li>
+        </ul>
+        
+        </footer>
     );
-  }
+    }
 
-  export default Footer;
-  
+export default Footer;
