973c90271cd95100df672fb26989925b0c7c3601
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-02-22 19:24:03 -0600

    fixing new errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import About from './pages/about.jsx';
-import Contact from './pages/Contact.jsx'
-import Resume from './pages/Resume.jsx'
-import Portfolio from './pages/Portfolio.jsx'; 
+import Contact from './pages/contact.jsx'
+import Resume from './pages/resume.jsx'
+import Portfolio from './pages/portfolio.jsx'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
