9db3d1ca77ce84457a8c29c10586d0ba310a1aa8
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-02-22 18:58:40 -0600

    trying to fix error in file structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import About from './pages/about.jsx'
-import Contact from './pages/contact.jsx'
-import Resume from './pages/resume.jsx'
-import Portfolio from './pages/portfolio.jsx'; 
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Resume from './pages/Resume.jsx'
+import Portfolio from './pages/Portfolio.jsx'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
